6330b5c5459e4f9dd3ad4e32981c2a90d22d82be
John Schulz <jschulz@twitter.com>
2014-04-02 20:43:37 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+(function() {
+    !function() {
+        var d3 = {
+            version: "3.4.4"
+        };
+        d3.min = function(array, f) {
+            var i = -1, n = array.length, a, b;
+            if (arguments.length === 1) {
+                while (++i < n && !((a = array[i]) != null && a <= a)) a = undefined;
+                while (++i < n) if ((b = array[i]) != null && a > b) a = b;
+            } else {
+                while (++i < n && !((a = f.call(array, array[i], i)) != null && a <= a)) a = undefined;
+                while (++i < n) if ((b = f.call(array, array[i], i)) != null && a > b) a = b;
+            }
+            return a;
+        };
+        if (typeof define === "function" && define.amd) {
+            define(d3);
+        } else if (typeof module === "object" && module.exports) {
+            module.exports = d3;
+        } else {
+            this.d3 = d3;
+        }
+    }();
+})();
